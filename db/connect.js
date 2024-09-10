2595b2f64837b4978f2a6f16cc8812721d0db67f
Rubinium <dench.240@yandex.ru>
2024-09-10 05:40:57 +0300

    update image on fass form

--- db/connect.js
+++ db/connect.js
@@ -2,7 +2,7 @@ const { mongoose } = require('mongoose')
 
 module.exports = async () => {
 	try {
-		await mongoose.connect(`${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`)
+		await mongoose.connect(`mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`)
 		console.log('Подключение к БД успешно')
 	} catch (e) {
 		console.error('Ошибка подключения БД')
