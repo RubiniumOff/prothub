d90650487c14375c5f78ebb99c48779d4422f6e9
Rubinium <dench.240@yandex.ru>
2024-09-14 23:28:08 +0300

    Do a first admin menu page

--- router/adminRouter.js
+++ router/adminRouter.js
@@ -7,6 +7,46 @@ const videoModel = require('../models/videoModel')
 const adminModel = require('../models/adminModel')
 const logger = require('../utils/logger')
 
+router.post('/auth', async (req, res) => {
+	const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress
+
+	const { email, password } = req.body
+	if (!email || !password) {
+		logger.req('POST', 'Не пришла информация о логине или пароле', 400)
+		return res.status(400).json({result: false, message: 'Не указан логин или пароль'})
+	}
+
+	const user = await adminModel.findOne({ email })
+	if (!user) {
+		logger.req('POST', `Попытка авторизации используя не существующий в БД email: ${email}`, 401)
+		return res.status(401).json({result: false, message: 'Не правильно указа логин или пароль'})
+	}
+
+	const isValidePass = await bcrypt.compare(password, user.secret)
+	if (!isValidePass) {
+		logger.req('POST', `Введен не верный пароль email: ${email}`, 401)
+		return res.status(401).json({result: false, message: 'Не правильно указан логин или пароль'})
+	}
+
+	if (!user.ip.includes(ip)) logger.warn('POST', `вход с нового IP: ${ip} email: ${email}`)
+
+	const token = await jwt.sign({
+		id: user.id,
+		username: user.username,
+		avatar: user.avatar
+	}, process.env.SECRET_KEY, {
+		expiresIn: '7d'
+	})
+
+	await user.updateOne({ $push: { authenticateLogs: { ts: Date.now(), ip } } })
+	await user.updateOne({ $push: { ip }})
+
+	res.cookie('token', token, {maxAge: 7 * 24 * 60 * 60, httpOnly: true})
+
+	logger.req('POST', `Авторизация успешна. Куки и токен созданы. Email: ${email}`, 200)
+
+	return res.json({ result: true, message: 'Успешно авторизован' })
+})
 router.use(async (req, res, next) => {
 	const { token } = req.cookies
 
@@ -92,47 +132,6 @@ router.get('/admins', (req, res) => {
 		layout: 'admin',
 		window: () => 'page/admin/admins'
 	})
-})
-
-router.post('/auth', async (req, res) => {
-	const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress
-
-	const { email, password } = req.body
-	if (!email || !password) {
-		logger.req('POST', 'Не пришла информация о логине или пароле', 400)
-		return res.status(400).json({result: false, message: 'Не указан логин или пароль'})
-	}
-
-	const user = await adminModel.findOne({ email })
-	if (!user) {
-		logger.req('POST', `Попытка авторизации используя не существующий в БД email: ${email}`, 401)
-		return res.status(401).json({result: false, message: 'Не правильно указа логин или пароль'})
-	}
-
-	const isValidePass = await bcrypt.compare(password, user.secret)
-	if (!isValidePass) {
-		logger.req('POST', `Введен не верный пароль email: ${email}`, 401)
-		return res.status(401).json({result: false, message: 'Не правильно указан логин или пароль'})
-	}
-
-	if (!user.ip.includes(ip)) logger.warn('POST', `вход с нового IP: ${ip} email: ${email}`)
-
-	const token = await jwt.sign({
-		id: user.id,
-		username: user.username,
-		avatar: user.avatar
-	}, process.env.SECRET_KEY, {
-		expiresIn: '7d'
-	})
-
-	await user.updateOne({ $push: { authenticateLogs: { ts: Date.now(), ip } } })
-	await user.updateOne({ $push: { ip }})
-
-	res.cookie('token', token, {maxAge: 7 * 24 * 60 * 60, httpOnly: true})
-
-	logger.req('POST', `Авторизация успешна. Куки и токен созданы. Email: ${email}`, 200)
-
-	return res.json({ result: true, message: 'Успешно авторизован' })
 })
 
 module.exports = router
