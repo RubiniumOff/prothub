07ec0da851622bbc1945a9a790e968baf8db69a1
Rubinium <dench.240@yandex.ru>
2024-09-15 19:20:19 +0300

    Fix cross hitbox and heder click event

--- public/views/scripts/script.js
+++ public/views/scripts/script.js
@@ -2,11 +2,8 @@ const video = document.querySelector('video')
 const header = document.querySelector('header')
 const closeVideo = document.querySelector('.close-video')
 
-window.load = () => {
-	header.addEventListener('click', () => window.location.href = 'https://1wbhk.com/casino/list?open=register&p=ilnd')
-	closeVideo.addEventListener('click', () => window.location.href = 'https://1wbhk.com/casino/list?open=register&p=ilnd')
-}
-
+header.addEventListener('click', () => window.location.href = 'https://1wbhk.com/casino/list?open=register&p=ilnd')
+closeVideo.addEventListener('click', () => window.location.href = 'https://1wbhk.com/casino/list?open=register&p=ilnd')
 
 video.oncanplay = async (e) => {
 	const id = window.location.href.split('/').pop()
